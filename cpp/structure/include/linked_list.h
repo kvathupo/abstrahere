9f6caf605237b34369117aaa04458351dc694a25
kvathupo <kalinda619@gmail.com>
2020-05-27 03:47:08 -0400

    working insertion

--- cpp/structure/include/linked_list.h
+++ cpp/structure/include/linked_list.h
@@ -1,28 +1,66 @@
 #pragma once
 
-namespace strct {
+#include <memory>
+
+inline namespace strct {
+
+// Definition
+// * Since you cannot separate a templated class into header and implementation,
+// it would be best to simply define the class with the declaration. 
 
 template<typename T>
 class LinkedList {
+public:
+    LinkedList(): sz{0} {}
+    void add(T input) {
+        if (this->head == nullptr) {
+            this->head = std::make_shared<Node>(input);
+            return;
+        }
+        std::shared_ptr<Node> curr = this->head;
+        std::shared_ptr<Node> next = curr;
+        while (next != nullptr) {
+            curr = next;
+            next = curr->getNext();
+        }
+        curr->setNext(std::make_shared<Node>(input));
+    }
+    void print() {
+        std::cout << "Starting print\n";
+        std::shared_ptr<Node> curr = this->head;
+        while (curr != nullptr) {
+            std::cout << curr->getContents() << "\n";
+            curr = curr->getNext();
+        } 
+        std::cout << "Ending print\n";
+    }
+    T remove(T);
+    void clear();
+    int size();
 private:
     class Node {
     private:
         T val;
-        Node *next;
+        std::shared_ptr<Node> next {};
     public:
-        virtual T getContents() = 0;
-        virtual Node *getNext() = 0;
-        ~Node ();                           // Don't intend for inheritance
-                                            // from base implementation, hence
-                                            // not declared virtual
+        Node(T dat): val{dat} {}
+        Node(T dat, Node next_node): val{dat},
+            next{std::make_shared<Node>(next_node)}
+        {}
+        T getContents() {
+            return this->val;
+        }
+        std::shared_ptr<Node> getNext() {
+            return next;                    // Does this bump refcount?
+                                            // I think so, but check with use_count
+        }
+        void setNext(std::shared_ptr<Node> in_node) {
+            next = in_node;
+        }
     }; 
-    Node *head;
+
+    std::shared_ptr<Node> head {};
     int sz;
-public:
-    virtual void add(T) = 0;
-    virtual T remove(T) = 0;
-    virtual void clear() = 0;
-    int size();
 };
 
 }
