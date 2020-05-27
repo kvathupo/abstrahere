35513dfd615b66535305a28ee4d068c83bb8a0c4
kvathupo <kalinda619@gmail.com>
2020-05-27 03:46:39 -0400

    init

---
+++ cpp/structure/test/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <memory>
+#include "linked_list.h"
+
+int main() {
+    std::cout << "At least I don't crash... yet...\n";
+    LinkedList<int> lst{};
+    lst.add(1);
+    lst.add(2);
+    lst.add(3);
+    lst.print();
+    return 0;
+}
