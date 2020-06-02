6b2dbd5cf25ff5b0d0198f65ade57c4f7224b361
kvathupo <kalinda619@gmail.com>
2020-06-02 06:02:49 -0400

    test copy-strctr

--- cpp/structure/test/main.cpp
+++ cpp/structure/test/main.cpp
@@ -8,11 +8,9 @@ int main() {
     lst.add(1);
     lst.add(2);
     lst.add(3);
-    lst.remove(2);
-    LinkedList<int> lst2 = std::move(lst);
-    lst.clear();
     std::cout << "Printing lst...\n";
     lst.print();
+    LinkedList<int> lst2 {lst};
     std::cout << "Printing lst2...\n";
     lst2.print();
     return 0;
