4d536ad56af9c7b66de66904192eeb20204fb36c
kvathupo <kalinda619@gmail.com>
2020-06-13 23:51:07 -0400

    test stack and linked list

--- cpp/structure/test/main.cpp
+++ cpp/structure/test/main.cpp
@@ -3,22 +3,23 @@
 #include "linked_list.h"
 #include "stack.h"
 
+strct::LinkedList<int> foo() {
+    strct::LinkedList<int> lst {};
+
+    lst.add(1);
+    lst.add(2);
+    lst.add(3);
+
+    return lst; 
+}
+
 int main() {
-    strct::Stack<int> s1 {};
-
-    s1.push(1);
-    s1.push(2);
-    s1.push(3);
-
-    std::cout << "Printing s1\n";
-    s1.print();
+    strct::LinkedList<int> lst = foo();
+    lst.print();
+    strct::LinkedList<int> lst2 = lst;
+    lst2.print();
+    lst.print();
     
-    std::cout << "Printing s2\n";
-    strct::Stack<int> s2 {s1};
-    s2.print();
-
-    std::cout << "Printing s1\n";
-    s1.print();
 
     return 0;
 }
